8aa1f26563eb7329d14c83de9aa79e7329bcbeee
karimorozova13 <k.morozova@tiomarkets.com>
2023-04-18 17:46:57 +0300

    add header icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,46 @@
 import React from "react";
 import styled from "styled-components";
+import {
+  FaFacebookF,
+  FaLinkedinIn,
+  FaTwitter,
+  FaInstagram,
+  FaEnvelope,
+} from "react-icons/fa";
+import { RiPhoneFill } from "react-icons/ri";
+import { MdLocationOn } from "react-icons/md";
 const Wrap = styled.div`
   padding: 30px;
-  background-color: #7a7a7a;
-  min-height: 300px;
+  background-color: #abb8c3;
+  display: flex;
 `;
-
+const Contacts = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+const SocialIcons = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin-left: auto;
+`;
 const Header = () => {
-  return <Wrap></Wrap>;
+  return (
+    <Wrap>
+      <Contacts>
+        <RiPhoneFill color={"#919191"} size={23} />
+        <FaEnvelope color={"#919191"} size={23} />
+        <MdLocationOn color={"#919191"} size={23} />
+      </Contacts>
+      <SocialIcons>
+        <FaFacebookF color={"#919191"} size={23} />
+        <FaLinkedinIn color={"#919191"} size={23} />
+        <FaTwitter color={"#919191"} size={23} />
+        <FaInstagram color={"#919191"} size={23} />
+      </SocialIcons>
+    </Wrap>
+  );
 };
 
 export default Header;
